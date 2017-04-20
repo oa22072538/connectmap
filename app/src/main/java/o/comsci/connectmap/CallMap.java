a95c0199aaf4f399679deb52895209bbaed2085a
oa22072538 <oa22072538oa22072538@hotmail.com>
2017-04-20 10:06:00 +0700

    Design UI

---
+++ app/src/main/java/o/comsci/connectmap/CallMap.java
@@ -0,0 +1,13 @@
+package o.comsci.connectmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+    } //Create Method
+}
